0c3a12e3a06b7f638304c8a08004e3b5833aeb1d
Tom Durrant <tomchdurrant@gmail.com>
2020-11-05 14:52:39 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    const THING: u32 = 200;
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    println!("The value of thing is: {}", THING);
+}
